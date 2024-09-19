9e5720aad4c0208a87f3424828b8e6128c714a5e
1RAFTIK1 <134624191+1RAFTIK1@users.noreply.github.com>
2024-09-20 00:27:31 +0300

    Update VehiclePack.java
    up

--- ElectricCar/VehiclePack.java
+++ ElectricCar/VehiclePack.java
@@ -1,10 +1,20 @@
-package vehicle;
 
-class ElectricCar extends Car {
-    int batteryCapacity;
+package vehicles;
 
-    @Override
-    public void setEngineType(String engineType) {
-        super.setEngineType(engineType);
+public class ElectricCar extends Car {
+    private int batteryCapacity; 
+
+    public ElectricCar(String ownerName, String insuranceNumber, int batteryCapacity) {
+        super(ownerName, insuranceNumber, "Electric"); 
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
     }
 }
+
