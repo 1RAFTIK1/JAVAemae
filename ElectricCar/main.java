eb9633d57bed9f7756a7007b11e5a9cd64d8c504
1RAFTIK1 <134624191+1RAFTIK1@users.noreply.github.com>
2024-09-21 10:35:24 +0300

    Update main.java

--- ElectricCar/main.java
+++ ElectricCar/main.java
@@ -5,12 +5,12 @@ import vehicles.ElectricCar;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("John Doe", "INS12345", "Gasoline");
+        Car car = new Car("Moby Dick", "88005553535", "Gas");
         System.out.println("Car Owner: " + car.getOwnerName());
         System.out.println("Car Insurance Number: " + car.getInsuranceNumber());
         System.out.println("Car Engine Type: " + car.getEngineType());
 
-        ElectricCar electricCar = new ElectricCar("Jane Smith", "EINS67890", 75);
+        ElectricCar electricCar = new ElectricCar("Jigurda mihalich", "6942011", 75);
         System.out.println("Electric Car Owner: " + electricCar.getOwnerName());
         System.out.println("Electric Car Insurance Number: " + electricCar.getInsuranceNumber());
         System.out.println("Electric Car Engine Type: " + electricCar.getEngineType());
